c9dd4a76ff7d92afdd3d58e6e8213f75cd6ef8eb
Cedric-M <cirdec95@gmail.com>
2019-05-21 14:29:38 +0200

    Création session bean Stateless SoapPaymentValidator exposant une locale

---
+++ src/main/java/com/store/business/logic/SoapPaymentValidator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.store.business.logic;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author ced
+ */
+@Stateless
+public class SoapPaymentValidator implements PaymentValidator {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
